af903b5a82ad24d8ac5c99c12e29f5962f18d57c
Sai Kiran <neelakantamsaikiran2@gmail.com>
2026-01-29 20:58:38 +0530

    Complete functions using numbers

--- index.go
+++ index.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, Go!")
+
+	age :=22   //types are locked at compile time
+	fmt.Println(age)
+	// age:="sai kiran"  // cant assigin
+
+
+	 result := add(1, 2)
+    fmt.Println(result)
 }
+
+
+func add(a int,b int ) int {
+	return a + b
+}
+
