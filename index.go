aff690a7985bb6b1bf764ffe57f7936dd5656425
Sai Kiran <neelakantamsaikiran2@gmail.com>
2026-01-31 12:48:22 +0530

    Completed struct

--- index.go
+++ index.go
@@ -12,10 +12,23 @@ func main() {
 
 	 result := add(1, 2)
     fmt.Println(result)
+	
+	user := User{    // assigiing data 
+        Name: "Kiran",
+		Age: 22,
+	}
+
+	fmt.Println(user)
 }
 
 
 func add(a int,b int ) int {
 	return a + b
+}
+
+
+type User struct {   // a blue print 
+	Name string
+	Age  int
 }
 
