4dee41d162f983bf112d5539925fae78afca7c07
Sai Kiran <neelakantamsaikiran2@gmail.com>
2026-02-01 17:43:11 +0530

    learn: forloop with 1 billon

--- main.go
+++ main.go
@@ -5,15 +5,26 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 
+	resultCh := make(chan int64)
+	
 
-func helloHandler(w http.ResponseWriter , r *http.Request){
-	fmt.Fprintln(w,"Hello")
+	go func() {
+		var sum int64 = 0
+		for i := int64(0); i < 1_000_000_000; i++ {
+			sum += i
+		}
+		resultCh <- sum
+	}()
+
+	sum := <-resultCh
+	fmt.Fprintln(w, "Hello", sum)
 }
 
 func main() {
-	http.HandleFunc("/hello",helloHandler)
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Println("Server running on http://localhost:8080")
-	http.ListenAndServe(":8080",nil)
+	http.ListenAndServe(":8080", nil)
 }
